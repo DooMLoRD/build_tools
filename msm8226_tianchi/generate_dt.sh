5816c60d9b144217fdc34e2f0856dfdc584468c5
DooMLoRD <metoo.mememe@gmail.com>
2014-03-19 00:27:02 +0530

    added build scripts for Sony tianchi board (msm8226)

---
+++ msm8226_tianchi/generate_dt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# path to kernel sources
+KERNEL_SRC_PATH=~/android_kernel_sony_msm8226/
+
+echo "generating device tree..."
+./dtbTool -o dt.img -s 2048 -p {KERNEL_SRC_PATH}/scripts/dtc/ {KERNEL_SRC_PATH}/arch/arm/boot/
