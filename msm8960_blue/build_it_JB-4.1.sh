e71945d8ad49f29a124d573f260dd7569073dac2
DooMLoRD <metoo.mememe@gmail.com>
2014-03-29 20:49:16 +0530

    fixes to build script for JB 4.1 Sony blue board (msm8960)

--- msm8960_blue/build_it_JB-4.1.sh
+++ msm8960_blue/build_it_JB-4.1.sh
@@ -1,1 +1,1 @@
-python mkelf.py -o kernel.elf zImage@0x80208000 ramdisk.cpio.gz@0x81400000,ramdisk RPM_JB.bin@0x00020000,rpm bootcmd_JB@0x00000000,cmdline
+python mkelf.py -o kernel.elf zImage@0x80208000 ramdisk.cpio.gz@0x81400000,ramdisk RPM_JB-4.1.bin@0x00020000,rpm bootcmd_JB-4.1@0x00000000,cmdline
