31f58d0d2208dc54906f08a8f01876fa0d473d96
abcdjdj <abcdjdj@gmail.com>
2014-06-14 21:01:23 +0530

    Force dtbTools V2 & some general fixes

--- msm8226_tianchi/generate_dt.sh
+++ msm8226_tianchi/generate_dt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # path to kernel sources
-KERNEL_SRC_PATH=~/android_kernel_sony_msm8226/
+KERNEL_SRC_PATH=~/android_kernel_sony_msm8226
 
 echo "generating device tree..."
-./dtbTool -o dt.img -s 2048 -p {KERNEL_SRC_PATH}/scripts/dtc/ {KERNEL_SRC_PATH}/arch/arm/boot/
+./dtbTool --force-v2 -o dt.img -s 2048 -p $KERNEL_SRC_PATH/scripts/dtc/ $KERNEL_SRC_PATH/arch/arm/boot/
